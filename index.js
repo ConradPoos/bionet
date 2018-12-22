2b21a87b89ccc18541e12980c0bb3eb23d402fcd
ConradPoos <39649591+ConradPoos@users.noreply.github.com>
2018-12-22 12:51:57 +0000

    Update index.js

--- index.js
+++ index.js
@@ -97,6 +97,7 @@ if(cmd === `${prefix}purge`) {
       .setTitle("Credits")
       .setColor(3447003)
       .addField("Lead Developer/Creator", "Conrad#0597")
+      .addField("Additional Developers", "Pixlfyre#0416")
       .addField("Retired Developer", "Tech#9818")
       .setFooter("bioNET made by Conrad#0597")
       message.channel.sendMessage(embed);
