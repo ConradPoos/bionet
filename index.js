67fbbcd985b75dc5b210bd282b813cb3bf584709
ConradPoos <39649591+ConradPoos@users.noreply.github.com>
2018-12-24 12:44:23 +0000

    Update index.js

--- index.js
+++ index.js
@@ -111,34 +111,34 @@ if(cmd === `${prefix}purge`) {
      if rngfood = 0 {
         message.channel.send(":pizza:");   
      }
-      if rngfood = 1 {
+      if(rngfood = 1) {
         message.channel.send(":hamburger:");   
      }
-      if rngfood = 2 {
+      if(rngfood = 2) {
         message.channel.send(":doughnut:");   
      }
-      if rngfood = 3 {
+      if(rngfood = 3) {
         message.channel.send(":apple:");   
      }
-      if rngfood = 4 {
+      if(rngfood = 4) {
         message.channel.send(":fries:");   
      }
-      if rngfood = 5 {
+      if(rngfood = 5) {
         message.channel.send(":bread:");   
      }
-      if rngfood = 6 {
+      if(rngfood = 6) {
         message.channel.send(":watermelon:");   
      }
-      if rngfood = 7 {
+      if(rngfood = 7) {
         message.channel.send(":grapes:");   
      }
-      if rngfood = 8 {
+      if(rngfood = 8) {
         message.channel.send(":avocado:");   
      }
-      if rngfood = 9 {
+      if(rngfood = 9) {
         message.channel.send(":cookie:");   
      }
-      if rngfood = 10 {
+      if(rngfood = 10) {
         message.channel.send(":chocolate_bar:");   
      }
   }
