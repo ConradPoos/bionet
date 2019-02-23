bd61f2dda6d6d2d4543544558dab185bc7312867
ConradPoos <39649591+ConradPoos@users.noreply.github.com>
2019-02-23 14:39:30 +0000

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ bot.on('message', async (message) => {
         song[0],
         {
 maxResults: 1,
-          key: envConfig.YTAPI,
+          key: process.env.YTAPI,
           type: "video"
         },
         function(err, results) {
